c820de91fa3d5838129a9b4f8b0459313afeb0e0
Luiz Fernando <luizfranca35mc@gmail.com>
2025-09-11 22:17:19 -0300

    Exercicio001
    Exemplos de javacript

---
+++ exercicio001/app.js
@@ -0,0 +1,9 @@
+let nome = ""
+let idade = 0
+nome = prompt("Informe seu nome: ")
+idade = prompt("Informe sua idade: ")
+alert("Ola, seja bem vindo(a) " + nome +"\nVoce tem " + idade + " anos ")
+
+console.log(("Ola, seja bem vindo(a) " + nome +"\nVoce tem " + idade + " anos "))
+
+document.write("Ola, seja bem vindo(a) " + nome +"\nVoce tem " + idade + " anos ")

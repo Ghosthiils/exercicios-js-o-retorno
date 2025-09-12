31511aa441d62885e82540eaacee9efedd376752
Luiz Fernando <luizfranca35mc@gmail.com>
2025-09-12 20:18:52 -0300

    Exemplos de contas

---
+++ exercicio002/app.js
@@ -0,0 +1,19 @@
+let n1 = 10, n2 = 2
+console.log(n1+n2) // + operador de adicao
+document.getElementById("resultado_soma").innerText = n1+n2
+
+console.log(n1-n2)
+document.getElementById("resultado_subtrai").innerText = n1-n2
+ // - operador de subtracao
+console.log(n1*n2)
+document.getElementById("resultado_multi").innerText = n1*n2
+ // * operador de multiplicacao
+console.log(n1/n2)
+document.getElementById("resultado_divisao").innerText = n1/n2
+ // / operador de divisao
+console.log(n1%n2)
+document.getElementById("resultado_resto").innerText = n1%n2 // % operador de modulo, pega o resto de divisao
+
+
+console.log(n1**n2)
+document.getElementById("resultado_expo").innerText = n1**n2 // operador de exponenciacao, pega um numero e eleva a outro 10.2

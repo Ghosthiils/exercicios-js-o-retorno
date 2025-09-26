e913c1b14134364e8e5b0284e2698fa88ceae0a5
Luiz Fernando <luizfranca35mc@gmail.com>
2025-09-25 22:15:37 -0300

    Exercicio 009 repetindo quantas frases eu escolher

---
+++ exercicio009/app.js
@@ -0,0 +1,18 @@
+function fnRepetirFrase() {
+    let qtd = 0, frases="", contador=0
+
+    
+    
+    frase = document.getElementById("frase").value
+    qtd = document.getElementById("qtd").value
+    document.getElementById("lista_frases").innerHTML = ""
+    while(contador < qtd){
+        document.getElementById("lista_frases").innerHTML += "<li>" + frase + "</li>"
+        contador++ 
+    }
+
+
+    
+
+
+}

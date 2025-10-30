e02d4f5b49f7d34c0bef264c6c42432f4736a8d0
Luiz Fernando <luizfranca35mc@gmail.com>
2025-10-30 19:56:44 -0300

    campo de postagem

--- exercicio015/app.js
+++ exercicio015/app.js
@@ -1,70 +1,51 @@
 let contatos = []
 
-let umVinicius = {
-    nome:'Vinicius',
-    telefone: '199999990',
-    email: 'vini@gmail.com',
-    foto:'v.jpg'
-    
-}
-let umGuto = {
-    nome:'Guto',
-    telefone: '1999997790',
-    email: 'guto@gmail.com',
-    foto:'g.jpg'
-    
-}
-
-
-
 function fnSalvar(){
-    let umContato = {
-        nome: document.getElementById('nome').value,
-        telefone: document.getElementById('telefone').value,
-        email: document.getElementById('email').value,
-        foto: document.getElementById('foto').value
+   let umContato = {
+nome: document.getElementById('nome').value,
+telefone: document.getElementById('telefone').value,
+email: document.getElementById('email').value,
+foto: document.getElementById('foto').value
+   }
+   contatos.push(umContato)
 
-        
-    }
-    contatos.push(umContato)
-
-    console.dir(contatos)
+   console.dir(contatos)
 }
 
 function fnListar(){
-    let dados =""
+let dados =""
+
     contatos.forEach(function(umContato,i){
-        dados +=
-    
-    `<div class="card" style="width: 18rem;">
-  <img src="${umContato.foto}" class="card-img-top" alt="${umContato.nome}">
-  <div class="card-body">
-    <h5 class="card-title">${umContato.nome}</h5>
-    <p class="card-text">${umContato.telefone} - ${umContato.email} </p>
-    <a href="#" class="btn btn-danger">Excluir</a>
-  </div>
-</div>`
-    })
-
-  
-
+        dados += `
+        <div class="card" style="width: 18rem;">
+        <img src="${umContato.foto}" class="card-img-top" alt="${umContato.nome}">
+        <div class="card-body">
+            <h5 class="card-title">${umContato.nome}</h5>
+            <p class="card-text">${umContato.telefone} -  ${umContato.email}</p>
+            <button type="button" onclick='fnExcluir(${i})' class="btn btn-danger">excluir</button>
+ 
+        </div>
+        </div>
         
+        `
+})
+
+document.getElementById(`listaDeContatos`).innerHTML = dados
+}
+
+function fnExcluir(indice){
+    contatos.splice(indice,1)
+    fnListar()
+}
+
+
+function fnLimpar(){
+    document.getElementById('formulario').reset()
+
     }
 
-    document.getElementById('listaDeContatos').innerHTML = dados
-
-
-    }
-
-
-
-function fnExcluir(){}
-
-
-   
-
-
-document.getElementById("btSalvar").addEventListener("click", function() {
+document.getElementById('btSalvar').addEventListener('click',function(){
     fnSalvar()
     fnListar()
+    fnLimpar()
 })
